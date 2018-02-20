87d179d889d3e3ff064d98363f63398b04fc9c7a
juunini <juuni.k.qr@gmail.com>
2018-02-20 14:42:11 +0900

    Add files via upload

--- lcd/i2c_dht.py
+++ lcd/i2c_dht.py
@@ -9,8 +9,24 @@ sensor = Adafruit_DHT.DHT11 # DHT version
 
 pin = 4 # BCM 4, WPi 7, pin7
 
+month_string = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+month_num = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
+
 while True:
     humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-    I2C.lcd_string("{}`C".format(temperature), I2C.LCD_LINE_1)
-    I2C.lcd_string("{}%".format(humidity), I2C.LCD_LINE_2)
+
+    time_split = time.ctime().split(" ")
+    year = time_split[4]
+    month = time_split[1]
+    day = time_split[2]
+    week = time_split[0]
+    current_time = time_split[3]
+
+    for i in range(0, 12):
+        if month == month_string[i]:
+            month = month_num[i]
+            break
+
+    I2C.lcd_string("{}.{}.  {}".format(month, day, current_time), I2C.LCD_LINE_1)
+    I2C.lcd_string("{}`C     {}%".format(temperature, humidity), I2C.LCD_LINE_2)
     time.sleep(1)
