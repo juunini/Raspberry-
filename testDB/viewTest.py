a06102818a50d1949ea609bc74357a597d89c7e5
juunini <juuni.k.qr@gmail.com>
2018-02-28 13:50:25 +0900

    Update viewTest.py

--- testDB/viewTest.py
+++ testDB/viewTest.py
@@ -3,12 +3,25 @@ import psycopg2 as db
 conn = db.connect("host=localhost dbname=dht11 user=postgres password=1234")
 cur = conn.cursor()
 
+cur.execute("""
+    SELECT column_name
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_NAME = ''
+""")
+describe = cur.fetchall()
+
+for desc in describe :
+    print(desc, end = '\t')
+    
+print('\n----------------------------------------------------')
+
 cur.execute("SELECT * FROM dht11")
 rows = cur.fetchall()
 
-print("date\t\t\ttemperature\thumidity")
-for row in rows:
-    print("{}\t{}\t\t{}".format(row[0], row[1], row[2]))
+for row in rows :
+    for r in row :
+        print(r, end = '\t')
+    print()
 
 cur.close()
 conn.close()
